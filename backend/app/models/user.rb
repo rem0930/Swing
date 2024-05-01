15c33069a107b041e24979de4122a8e941663052
rem0930 <06araki@gmail.com>
2024-05-02 03:14:02 +0900

    make_login

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
 
     # JWTトークン生成メソッド
     def generate_jwt
-        JWT.encode({ id: self.id, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
+        JWT.encode({ id: self.id, exp: 24.hours.from_now.to_i }, Rails.application.credentials.jwt_secret_key)
     end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'signup', to: 'users#create'
+  post 'login', to: 'sessions#create'
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
