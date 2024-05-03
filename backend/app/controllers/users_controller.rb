9c649ed78df7638101ddab351ccd0aeea14b5e69
rem0930 <06araki@gmail.com>
2024-05-04 01:42:44 +0900

    create_signup

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -8,26 +8,26 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       render json: { status: "User created successfully" }, status: :created
     else
-      render json: { errors: user_errors(user) }, status: :bad_request
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  private
+    private
 
-  def user_params
-    params.permit(:email, :password, :user_name, :profile_photo, :background_photo, :bio)
-  end
+    def user_params
+      params.required(:user).permit(:email, :password, :user_name, :profile_photo, :background_photo, :bio)
+    end
 
-  def user_errors(user)
-    errors = []
-    errors << "Email has already been taken" if user.errors[:email].present?
-    errors << "Password is too short (minimum is 6 characters)" if user.errors[:password].present?
-    errors << "User name is required" if user.errors[:user_name].present?
-    # 他のエラーチェックも追加可能
-    errors
-  end
+    def user_errors
+      errors = []
+      errors << "Email has already been taken" if @user.errors[:email].present?
+      errors << "Password is too short (minimum is 6 characters)" if @user.errors[:password].present?
+      errors << "User name is required" if @user.errors[:user_name].present?
+      # 他のエラーチェックも追加可能
+      errors
+    end
 end
