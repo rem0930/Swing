ae11a3bed9bf88689db224fa8ea9a6e9566e4b18
EC2 Default User <ec2-user@ip-10-0-1-78.ap-northeast-1.compute.internal>
2024-07-17 02:16:49 +0000

    modify cd

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -53,4 +53,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << "54.249.6.3"
 end
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,22 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_GEOCODING_API_KEY: process.env.NEXT_PUBLIC_GEOCODING_API_KEY,
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID: process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID,
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_REGION: process.env.AWS_REGION,
-    AWS_BUCKET: process.env.AWS_BUCKET,
-  },
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
-
-  // ヘッダーの設定
   async headers() {
     return [
       {
@@ -30,8 +16,6 @@ const nextConfig = {
       },
     ];
   },
-
-  // Webpack設定（開発環境のみ）
   webpack(config, { dev }) {
     if (dev) {
       config.watchOptions = {
@@ -44,3 +28,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
